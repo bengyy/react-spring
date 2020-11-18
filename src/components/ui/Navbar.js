c5c13433ae0a928a21ea8bc799e147621d0521ed
BenitoMartinez <viajero18812@innovaccion.mx>
2020-11-18 09:02:26 -0600

    dev16

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -12,9 +12,7 @@ export const Navbar = () => {
     }
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <span className="navbar-brand">
-                { name }
-            </span>
+            
             
             <Link 
                 className="navbar-brand" 
